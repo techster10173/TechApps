c25075831643e21ec41cf6c50ea1273674de607e
Sree Grandhe <sreegrandhe@live.com>
2019-01-17 10:32:57 -0600

    PHP Assignments

---
+++ PHP/PHPandHTML/all_posts.php
@@ -0,0 +1,11 @@
+<!doctype HTML>
+<?php
+$title = “Awesome Page”;
+include(“header.php”);
+?>
+
+<div>
+
+Whatevs
+
+</div>
---
+++ PHP/PHPandHTML/header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html>
+
+<head>
+
+<title><?php
+echo $title; ?></title>
+
+</head>
+
+<body>
+
+<div>
+
+This is
+</div>
