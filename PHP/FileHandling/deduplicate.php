9c6fcb79c6935f843f99d4491c9427cfc80d89b5
Sree Grandhe <sreegrandhe@gmail.com>
2019-02-05 00:21:14 -0600

    Finished Deduplicate

--- PHP/FileHandling/deduplicate.php
+++ PHP/FileHandling/deduplicate.php
@@ -1,15 +1,30 @@
 <?php
+$rFiles = $_GET["og"];
+$dir = dirname($rFiles);
+    //echo $dir;
 // Grab all files from the desired folder
-$files = glob("./OrganizedFiles/1992/01/*.*");
+$files = glob($dir . "/*.*");
 
-// Sort files by modified time, latest to earliest
-// Use SORT_ASC in place of SORT_DESC for earliest to latest
+$temp = array();
+
+for ($i=0; $i < count($files) ; $i++) {
+    if(file_get_contents($rFiles) == file_get_contents($files[$i])){
+        array_push($temp, $files[$i]);
+    }
+}
+
 array_multisort(
-array_map( 'filemtime', $files ),
+array_map('filemtime', $files ),
 SORT_NUMERIC,
 SORT_DESC,
-$files
+$temp
 );
 
-echo $files[0];
+for ($i=count($temp)-1; $i >=1 ; $i--) {
+    unlink($temp[$i]);
+}
+
+
  ?>
+
+<!-- http://localhost:8000/deduplicate.php?og=./OrganizedFiles/1992/01/islandcoast.txt -->
