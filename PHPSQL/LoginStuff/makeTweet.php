773b288f18880ff1eea1d8459c683cacc86d3d93
Sree Grandhe <sreegrandhe@gmail.com>
2019-04-03 10:21:44 -0500

    Insert hashtags into databse

--- PHPSQL/LoginStuff/makeTweet.php
+++ PHPSQL/LoginStuff/makeTweet.php
@@ -21,8 +21,27 @@ if($conn->connect_error)
 $stmt = $conn->prepare("INSERT INTO tweets (content,userid) VALUES (?,?)");
 $stmt->bind_param("si", $content, $_SESSION["myID"]);
 $stmt->execute();
+$bello = $conn->insert_id;
 
-preg_match_all('/(#\w+)/', $content, $matches);
+$keywords;
+preg_match_all('/#(\w+)/',$content,$matches);
+foreach($matches[1] as $match){
+
+    $stmt = $conn->prepare("INSERT INTO hashtags (content) VALUES (?)");
+    $stmt->bind_param("s", $match);
+    $stmt->execute();
+
+    $stmt->close();
+
+    $hello = $conn->insert_id;
+    $yolo = $conn->prepare("INSERT INTO hashtagstotweets (tweetid,hashtagid) VALUES (?,?)");
+    $yolo->bind_param("ii", $bello, $hello);
+    $yolo->execute();
+}
+
+foreach($keywords as $key){
+
+}
 
 header("Location:home.php");
 
