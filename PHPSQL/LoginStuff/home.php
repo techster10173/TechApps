2d721e0cc10c034dbb649e05c6dc1bac885d52e3
Sree Grandhe <sreegrandhe@gmail.com>
2019-04-03 21:58:34 -0500

    Finished hashtags

--- PHPSQL/LoginStuff/home.php
+++ PHPSQL/LoginStuff/home.php
@@ -22,7 +22,12 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+    <style>
+    a:link { color: #000000; text-decoration: none}
+a:visited { color: #000000; text-decoration: none}
+a:hover { color: #3366CC; text-decoration: underline}
+a:active { color: #000000; text-decoration: none}
+    </style>
 </head>
 <nav class="navbar navbar-light bg-light justify-content-between">
   <form class="form-inline">
@@ -84,10 +89,11 @@ $sql = "SELECT tweets.id,users.username,tweets.content,tweets.timer FROM tweets 
 $result = $conn->query($sql);
 date_default_timezone_set('UTC');
 while($row = $result->fetch_assoc()){
+    $text = preg_replace('/(?<!\S)#([0-9a-zA-Z]+)/', '<a href="hashtag.php?tag=$1">#$1</a>', $row["content"]);
     echo "<div class='card text-white bg-primary mb-5' style='max-width: 18rem;'>";
     echo "<div class='card-header'><a style='color:white' href='user.php?name=" . $row["username"] . "'>" . "@" . $row["username"] . "</a></div>";
     echo "<div class='card-body'>";
-    echo "<h5 class='card-title'>" . $row["content"] . "</h5>";
+    echo "<h5 class='card-title'>" . $text . "</h5>";
     echo "<p>" . date($row["timer"]) . "</p>";
     echo "<a style='color:white' href='tweets.php?id=" . $row["id"] . "'>See Full</a>";
     echo "</div></div>";
